d6c8b5ebda63578900b32c0349db7af895902e51
NTTPS <teamerze@gmail.com>
2024-09-28 01:23:20 +0700

    delete dist

--- dist/types/src/utils/plugins/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { App as Application, Component } from 'vue';
-import { Defaults } from '../defaults';
-export declare const registerComponent: (instance: Application, component: Component, defaults?: Defaults) => void;
-export declare const registerComponentProgrammatic: (instance: Application, property: string, component: Component) => void;
